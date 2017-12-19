8ac02992f1f30b4c8d5a142ef368766e4d333ab3
sk8terkev777 <31445473+sk8terkev777@users.noreply.github.com>
2017-12-19 15:15:30 -0500

    Add files via upload

---
+++ thrustAmount2.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class thrustAmount2 : MonoBehaviour {
+
+
+	Rigidbody rb;
+	public float thrustAmt2; 
+	public AudioSource testSound;
+	public AudioClip Theme;
+	public GameObject Pin1;
+	public GameObject Mii;
+	public Vector3 startingPostion;
+	public GameObject DestroyMe;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			rb.AddForce (Vector3.forward * thrustAmt2);
+			testSound.PlayOneShot (Theme, 0.7f);
+		}
+
+		if(Input.GetKey (KeyCode.DownArrow)){
+			rb.AddForce (Vector3.back * thrustAmt2);
+
+		}
+
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			rb.AddForce (Vector3.left * thrustAmt2);
+		}
+
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			rb.AddForce (Vector3.right * thrustAmt2);
+		}
+
+	
+		
+	}
+}
---
+++ time action.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeaction : MonoBehaviour {
+
+	public float startTime = 5f;
+	public float interval =2f;
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Time.time > -startTime) {
+			Debug.Log ("it works");
+		}
+	}
+}
